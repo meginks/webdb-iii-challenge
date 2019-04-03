2f54df565ee6e05eb0335f1992d61a714732b83e
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-04-03 17:46:13 -0500

    added db, added tables, added 2 endpoints

---
+++ helperfunctions.js
@@ -0,0 +1,13 @@
+
+// const error500 = (error) => {
+//    res.status(500)
+//     .json({
+//         message: `Error! ${error}`
+//     })
+// } 
+
+// that was a fail
+
+// module.exports = { error500 }; 
+
+
